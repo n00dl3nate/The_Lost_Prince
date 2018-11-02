551d70da35090c6a1db0c4842bf2bfd7f553fe21
Nathan Atkinson <n00dl3nate@gmail.com>
2018-11-02 13:50:56 +0000

    added geting monster from api

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,10 @@
-const Game = require('./models/game.js');
+const Monster = require('./models/monster.js');
+const PubSub = require('./helpers/pub_sub.js');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  console.log("DOMContentLoaded");
+  // const choice = 1
+  // PubSub.publish('Monster:monster-choice',choice);
+  const monster = new Monster;
+  monster.bindEvents()
+});
