04088d0ad5875f21c6b8331c7c46d1bcbd65f825
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-11-02 14:35:48 +0000

    commit

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,6 @@
-const Monster = require('./models/monster.js');
+const Monster = require('./models/monster_model.js');
 const PubSub = require('./helpers/pub_sub.js');
+const RoomGenerator = require('./models/room_model.js');
 
 document.addEventListener('DOMContentLoaded', ()=>{
   console.log("DOMContentLoaded");
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   // PubSub.publish('Monster:monster-choice',choice);
   const monster = new Monster;
   monster.bindEvents()
-  
+
   const roomCreate = new RoomGenerator();
   roomCreate.bindEvents();
 });
