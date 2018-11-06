8a494419bb325e6e3a014e29f5259a5bd7b5e333
smelge <traildogfilms@gmail.com>
2018-11-06 17:12:35 +0000

    Player takes damage, balanced values

--- client/src/models/fight_model.js
+++ client/src/models/fight_model.js
@@ -17,26 +17,23 @@ Fight.prototype.playerAttack = function (monster){
   //monster declaration
   const monsterName = monster.name;
   const monsterAttack = monster.attack;
+  let monsterHp = monster.hp;
+  let yourResult = '';
+
   //Dice Roll Values Per Attack
   const playerAtk = this.player.getAttackHtml();
   const playerRoll = this.roll() + playerAtk;
   const enemyRoll = this.roll() + monsterAttack;
-  //Damage for monster
-  let monsterDamage = playerRoll - enemyRoll;
-  let monsterHp = (monster.hp - monsterDamage)
-  //Result declaration
-  let yourResult = '';
-  //Deciding attack outCome Player Attack
+
   if (playerRoll == enemyRoll){
     yourResult = `You attacked the ${monsterName}. It parried!`;
+  } else if (playerRoll < enemyRoll){
+    yourResult = `You attacked the ${monsterName}. You rolled [${playerRoll}] and it rolled [${enemyRoll}]. You failed to hurt it.`
+  } else {
+    var fightDamage = playerRoll - enemyRoll;
+    monsterHp -= fightDamage;
+    yourResult = `You attacked the ${monsterName}. You rolled [${playerRoll}] and it rolled [${enemyRoll}]. It took ${fightDamage} Damage! It has ${monsterHp} HP left.`;
   }
-  if (monsterHp <= 0){
-    yourResult = `You attacked the ${monsterName}. You rolled [${playerRoll}] and it rolled [${enemyRoll}]. It took ${monsterDamage} Damage! The Monster Is Dead!`;
-  }
-  else {
-    yourResult = `You attacked the ${monsterName}. You rolled [${playerRoll}] and it rolled [${enemyRoll}]. It took ${monsterDamage} Damage! Monster Hp:${monsterHp}`;
-  };
-
 
   const newMonster = {
     name: monster.name,
@@ -52,40 +49,35 @@ Fight.prototype.monsterAttack = function (monster) {
   //monster declaration
   const monsterName = monster.name;
   const monsterAttack = monster.attack;
-  var monsterHp = monster.hp;
-  //Dice Roll Values Per Attack
   const playerAtk = this.player.getAttackHtml();
-  const playerRoll = this.roll() + playerAtk;
-  const enemyRoll = this.roll() + monsterAttack;
-  //Damage for monster
-  let monsterDamage = playerRoll - enemyRoll;
-  //Result declaration
-  let result = '';
-  //player
-  let playerhp = (this.player.getHpHtml() - monsterDamage);
-  this.player.updateHp(playerhp)
-  //Deciding attack outCome Player Attack
-  if (playerRoll == enemyRoll){
-    theirResult = `The ${monsterName} attacked you, but you parried!`;
-  }
-  if (playerhp <= 0){
-    theirResult = `The ${monsterName} attacked you. It rolled [${monsterName}] and you rolled [${playerRoll}]. You take ${monsterDamage} Damage! You Are Dead`;
-    //Dead screen function
-  }
-  else {
-    theirResult = `The ${monsterName} attacked you. It rolled [${monsterName}] and you rolled [${playerRoll}]. You take ${monsterDamage} Damage!`
-  };
+  var playerRoll = this.roll() + playerAtk;
+  var enemyRoll = this.roll() + monsterAttack;
 
-  if (monsterHp <= 0){
-    var additional = `The ${monsterName} is dead.`;
-    this.enableNavigation();
-    return `${theirResult} ${additional}`;
+
+  var revengeResult = '';
+  var fightDamage = 0;
+
+  if (enemyRoll == playerRoll){
+    revengeResult = `The ${monsterName} attacked you, but you parried!`;
+  } else if (enemyRoll > playerRoll){
+    fightDamage = enemyRoll - playerRoll;
+    revengeResult = `The ${monsterName} attacked you. It rolled [${enemyRoll}] and you rolled [${playerRoll}]. You take ${fightDamage} Damage!`;
+    // Update player HP
+    var newPlayerHp = this.player.getHpHtml() - fightDamage;
+    this.player.updateHp(newPlayerHp);
+
   } else {
-    return theirResult;
+    revengeResult = `The ${monsterName} attacked you. It rolled [${enemyRoll}] and you rolled [${playerRoll}]. It failed to hurt you physically, but emotionally you are devastated.`
   }
-
-
-
+  //
+  // if (monsterHp <= 0){
+  //   var additional = `The ${monsterName} is dead.`;
+  //   this.enableNavigation();
+  //   return `${theirResult} ${additional}`;
+  // } else {
+  //   return theirResult;
+  // }
+  return revengeResult;
 }
 
 Fight.prototype.startFight = function (monster) {
